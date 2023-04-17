dcb7906670f2287afd8f14249f4cd94b6cb160ba
william.cox124 <william.cox24@d155.org>
2023-04-17 11:35:59 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(2500,2500);
+        frame.setResizable(false);
+
+
+
+        frame.setVisible(true);
+    }
+}
